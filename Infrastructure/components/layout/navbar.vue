a3f4abfc6187a902f92be4e8ce4f224a5b0d7723
Joao Vinicius <joaoviniciusdp@gmail.com>
2020-08-18 11:13:32 -0300

    Refactoring default layout component

---
+++ Infrastructure/components/layout/navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app-bar
+    app
+    absolute
+    elevate-on-scroll
+  >
+    <v-toolbar-title>
+      SimpleFlix
+    </v-toolbar-title>
+
+    <v-spacer />
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Navbar'
+})
+</script>
