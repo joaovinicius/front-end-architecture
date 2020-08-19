88fae58bc56fe032a702138b667086dc490152f7
Joao Vinicius <joaoviniciusdp@gmail.com>
2020-08-19 03:49:15 -0300

    Refactoring catalog entity

--- Domain/Entity/Catalog.ts
+++ Domain/Entity/Catalog.ts
@@ -1,15 +1,30 @@
-import { IMovie } from './Movie'
+interface Movie {
+  poster_path: string,
+  adult: boolean,
+  overview: string,
+  release_date: string,
+  genre_ids: Array<number>,
+  id: number,
+  original_title: string,
+  original_language: string,
+  title: string,
+  backdrop_path: string,
+  popularity: number,
+  vote_count: number,
+  video: boolean,
+  vote_average: number
+}
 
 export interface ICatalog {
   page: number,
-  results: Array<IMovie>,
+  results: Array<Movie>,
   total_results: number,
   total_pages: number
 }
 
 export class Catalog implements ICatalog {
   public page: number
-  public results: Array<IMovie>
+  public results: Array<Movie>
   public total_results: number
   public total_pages: number
 
