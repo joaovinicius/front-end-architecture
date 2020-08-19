8df8faa78b63f19d9c74da024f7315a62b61bc27
Joao Vinicius <joaoviniciusdp@gmail.com>
2020-08-19 03:53:18 -0300

    Add all routes utilized in use cases

--- Domain/Support/TheMovieDbRoutes.ts
+++ Domain/Support/TheMovieDbRoutes.ts
@@ -7,20 +7,48 @@ class TheMovieDbRoutes {
     this.apiKey = apiKey
   }
 
-  get authentication () {
-    return `${this.baseUrl}/authentication`
+  authentication (uri: string): string {
+    return `${this.baseUrl}/authentication/${uri}?api_key=${this.apiKey}`
   }
 
-  get authenticationTokenNew () {
-    return `${this.authentication}/token/new?api_key=${this.apiKey}`
+  get createRequestToken (): string {
+    return this.authentication('token/new')
   }
 
-  get authenticationWithLogin () {
-    return `${this.authentication}/token/validate_with_login?api_key=${this.apiKey}`
+  get createRequestTokenWithLogin (): string {
+    return this.authentication('token/validate_with_login')
+  }
+
+  get createSession() : string {
+    return this.authentication('session/new')
+  }
+
+  account (sessionId: string): string {
+    return `${this.baseUrl}/account?api_key=${this.apiKey}&session_id=${sessionId}`
+  }
+
+  accountWatchlist(accountId: string, sessionId: string, page: number): string {
+    return `${this.baseUrl}/account/${accountId}/watchlist/movies?api_key=${this.apiKey}&session_id=${sessionId}&page=${page}`
+  }
+
+  addToAccountWatchlist(accountId: string, sessionId: string): string {
+    return `${this.baseUrl}/account/${accountId}/watchlist?api_key=${this.apiKey}&session_id=${sessionId}`
+  }
+
+  popularMovies(page: number): string {
+    return `${this.baseUrl}/movie/popular?api_key=${this.apiKey}&page=${page}`
+  }
+
+  movieDetails(movieId: number): string {
+    return `${this.baseUrl}/movie/${movieId}?api_key=${this.apiKey}`
+  }
+
+  searchMovie(query: string, page: number): string {
+    return `${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${query}&page=${page}`
   }
 }
 
 const apiBaseUrl = process.env.NUXT_ENV_API_BASE_URL || ''
 const apiKey = process.env.NUXT_ENV_API_KEY || ''
 
-export const instance = new TheMovieDbRoutes(apiBaseUrl, apiKey)
+export const TMDBRoutes = new TheMovieDbRoutes(apiBaseUrl, apiKey)
