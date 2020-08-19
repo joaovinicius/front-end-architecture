75f6819ca2ce753be5ef0055674ba557456eebc7
Joao Vinicius <joaoviniciusdp@gmail.com>
2020-08-19 03:52:46 -0300

    Refactoring use case factory

--- Domain/Support/UseCaseFactory.ts
+++ Domain/Support/UseCaseFactory.ts
@@ -1,11 +1,8 @@
 import { IHttpClient } from './IHttpClient'
 
 export function UseCaseFactory (
-  httpClient: IHttpClient,
   Repository: any,
   UseCase: any
 ) {
-  const repositoryInstance = new Repository(httpClient)
-
-  return new UseCase(repositoryInstance)
+  return new UseCase(Repository)
 }
