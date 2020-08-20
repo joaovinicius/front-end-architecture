8e58508b2872c0f25086c67c7dde8feb225732ec
Joao Vinicius <joaoviniciusdp@gmail.com>
2020-08-20 04:40:47 -0300

    Add darkmode to layout

--- Infrastructure/nuxt.config.js
+++ Infrastructure/nuxt.config.js
@@ -1,3 +1,5 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   /*
    ** Nuxt rendering mode
@@ -75,7 +77,20 @@ export default {
    */
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {}
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    }
   },
   /*
    ** Build configuration
